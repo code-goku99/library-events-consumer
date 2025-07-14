0e52d9271e5c104856220a849b614b8a671d4587
sandeep <sandeepv2612@gmail.com>
2025-07-14 21:24:43 +0530

    Sample setup for consumer

---
+++ library-events-consumer/src/main/java/com/decay/kafka/config/LibraryEventsConfig.java
@@ -0,0 +1,9 @@
+package com.decay.kafka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+//@EnableKafka -> not need in latest version of kafka
+public class LibraryEventsConfig {
+}
---
+++ library-events-consumer/src/main/java/com/decay/kafka/consumer/LibraryEventsConsumer.java
@@ -0,0 +1,16 @@
+package com.decay.kafka.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class LibraryEventsConsumer {
+
+    @KafkaListener(topics = {"${kafka.topic}"})
+    public void onMessage(ConsumerRecord<Integer, String> consumerRecord) {
+        log.info("Consumer Message {}", consumerRecord);
+    }
+}
